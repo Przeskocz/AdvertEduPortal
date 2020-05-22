ffe7b4e1d6bc1ecbf071a80c4dbb655a535dca5b
przeskocz <skoczp@gmail.com>
2020-05-22 21:15:37 +0200

    Modele transmisji danych DTO

---
+++ src/main/java/com/przeskocz/AdvertEduPortal/model/DTO/CityDTO.java
@@ -0,0 +1,13 @@
+package com.przeskocz.AdvertEduPortal.model.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CityDTO {
+    private Long id;
+    private String named;
+}
